1ad7573c4b36c7b8776a0457e5353e60cc47ed5c
Matteo Pio Napolitano <matteopio.napolitano@gmail.com>
2018-12-23 20:53:22 +0100

    testing CircleCI + SFDX - test 15

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -3,7 +3,7 @@ import { LightningElement, track } from 'lwc';
 export default class HelloWorld extends LightningElement {
     @track firstName = 'User';
     @track lastName = 'Connected';
-    
+
     changeFirstnameHandler(event) {
         this.firstName = event.target.value;
     }
