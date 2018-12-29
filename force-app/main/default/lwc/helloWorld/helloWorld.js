b1795e95ad0fd503a4297d54be7e01419c5a6d18
Matteo Pio Napolitano <matteopio.napolitano@gmail.com>
2018-12-29 23:18:34 +0100

    removed last name from helloWorld component

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,13 +2,8 @@ import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     @track firstName = 'User';
-    @track lastName = 'Connected';
 
     changeFirstnameHandler(event) {
         this.firstName = event.target.value;
-    }
-
-    changeLastnameHandler(event) {
-        this.lastName = event.target.value;
     }
 }
